f136619473b86f52d820bdd67290a73d06bce678
musamer <aliathathi@iMac-alkhas-b-Ali.local>
2017-05-22 13:15:53 +0300

    1st Edit
    هذا التعديل الأول

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title>مؤشر الأداء</title>
+        <link rel="stylesheet" href="mystyle.css" />
+    </head>
+    <body>
+        <div class="container">
+            
+        </div>
+    </body>
+
+</html>
